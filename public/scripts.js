04f02870ea8da152f997d8bbd1981b193fdde7e6
Spenser Leighton <spenser.leighton@gmail.com>
2018-09-04 10:49:03 -0600

    Display ideas

--- public/scripts.js
+++ public/scripts.js
@@ -1,6 +1,7 @@
 const submitBtn = $('#submit-btn')
 submitBtn.on('click', postIdea)
 
+fetchIdeas()
 
 function postIdea(e) {
   e.preventDefault()
@@ -9,15 +10,40 @@ function postIdea(e) {
 
   let idea = { title, body }
 
-  return fetch('/api/v1/ideas', {
+  fetch('/api/v1/ideas', {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
       },
       body: JSON.stringify(idea)
     })
-      // .then(response => response.json())
-      .then(a => console.log(a))
+      .then(response => response.json())
+      .then(a => fetchSingleIdea(a.id))
+      .then(idea => displayIdeas(idea))
+  // $('#idea-form').reset()
+  // return fetchIdeas()
 
 }
 
+function fetchIdeas() {
+  return fetch('/api/v1/ideas')
+    .then(response => response.json())
+    .then(ideas => displayIdeas(ideas))
+}
+
+function fetchSingleIdea(id) {
+  return fetch(`/api/v1/ideas/${id}`)
+    .then(response => response.json())
+    .then(idea => idea)
+}
+
+function displayIdeas(ideas) {
+  return ideas.map(idea => {
+    $('#displayed-ideas').append(`
+      <article id="${idea.id}">
+        <h2>${idea.title}</h2>
+        <p>${idea.body}</p>
+      </article>
+    `)
+  })
+}
